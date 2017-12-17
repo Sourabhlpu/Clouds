0b15cea0606b19e0e372ee046f3dc10c535e27c8
Sourabh Pal <nick.tdr19@gmail.com>
2017-12-17 13:08:45 +0530

    Adding  NetModule to provide retrofit and weatherClient.

---
+++ app/src/main/java/com/example/personal/clouds/di/modules/NetModule.java
@@ -0,0 +1,43 @@
+package com.example.personal.clouds.di.modules;
+
+import com.example.personal.clouds.utilities.network.WeatherClient;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by personal on 12/17/2017.
+ */
+
+
+@Module
+public class NetModule {
+    String mBaseUrl;
+
+    public NetModule(String baseUrl)
+    {
+        mBaseUrl = baseUrl;
+    }
+
+    @Provides
+    @Singleton
+    Retrofit provideRetrofit()
+    {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(mBaseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        return retrofit;
+    }
+
+    @Provides
+    @Singleton
+    WeatherClient provideWeatherClient(Retrofit retrofit)
+    {
+        return retrofit.create(WeatherClient.class);
+    }
+}
