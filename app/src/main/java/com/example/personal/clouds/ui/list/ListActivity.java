d822c3040be33528770959f93897727ef7dd85ce
Sourabh Pal <nick.tdr19@gmail.com>
2018-01-05 10:34:25 +0530

    No comments.

--- app/src/main/java/com/example/personal/clouds/ui/list/ListActivity.java
+++ app/src/main/java/com/example/personal/clouds/ui/list/ListActivity.java
@@ -21,7 +21,7 @@ public class ListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_forecast);
 
         Clouds.getNetComponent().inject(this);
 
