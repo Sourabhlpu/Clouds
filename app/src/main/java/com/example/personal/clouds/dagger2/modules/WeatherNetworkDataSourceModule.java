6e238e2819c0c171135cfc3d4b6f640cd5db9475
Sourabh Pal <nick.tdr19@gmail.com>
2018-02-02 22:06:32 +0530

    Adding AppExecutors providers in the module.

--- app/src/main/java/com/example/personal/clouds/dagger2/modules/WeatherNetworkDataSourceModule.java
+++ app/src/main/java/com/example/personal/clouds/dagger2/modules/WeatherNetworkDataSourceModule.java
@@ -1,9 +1,16 @@
 package com.example.personal.clouds.dagger2.modules;
 
 import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
 
+import com.example.personal.clouds.AppExecutors;
 import com.example.personal.clouds.data.network.WeatherClient;
 import com.example.personal.clouds.data.network.WeatherNetworkDataSource;
+
+import java.util.concurrent.Executors;
+
+import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
@@ -23,5 +30,27 @@ public class WeatherNetworkDataSourceModule {
         return new WeatherNetworkDataSource(client,context);
     }
 
+    @Singleton
+    @Provides
+    Handler providesHandler()
+    {
+        return new Handler(Looper.getMainLooper());
+    }
+
+    @Singleton
+    @Provides
+    AppExecutors.MainThreadExecutor getMainThreadExecutor()
+    {
+        return new AppExecutors.MainThreadExecutor();
+    }
+
+    @Singleton
+    @Provides
+    AppExecutors getAppExecutors(AppExecutors.MainThreadExecutor mainThreadExecutor)
+    {
+        return new AppExecutors(Executors.newSingleThreadExecutor(),
+                Executors.newFixedThreadPool(3)
+                ,mainThreadExecutor);
+    }
 
 }
