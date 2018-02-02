c03fbfaac29943467bdfb526a46f0ef602c6fca0
Sourabh Pal <nick.tdr19@gmail.com>
2018-02-02 22:08:16 +0530

    Adding the previous provider in the correct Module.

--- app/src/main/java/com/example/personal/clouds/dagger2/modules/WeatherNetworkDataSourceModule.java
+++ app/src/main/java/com/example/personal/clouds/dagger2/modules/WeatherNetworkDataSourceModule.java
@@ -1,16 +1,9 @@
 package com.example.personal.clouds.dagger2.modules;
 
 import android.content.Context;
-import android.os.Handler;
-import android.os.Looper;
 
-import com.example.personal.clouds.AppExecutors;
 import com.example.personal.clouds.data.network.WeatherClient;
 import com.example.personal.clouds.data.network.WeatherNetworkDataSource;
-
-import java.util.concurrent.Executors;
-
-import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
@@ -23,34 +16,8 @@ import dagger.Provides;
 public class WeatherNetworkDataSourceModule {
 
 
-
     @Provides
-    public WeatherNetworkDataSource weatherNetworkDataSource(WeatherClient client, Context context)
-    {
-        return new WeatherNetworkDataSource(client,context);
+    public WeatherNetworkDataSource weatherNetworkDataSource(WeatherClient client, Context context) {
+        return new WeatherNetworkDataSource(client, context);
     }
-
-    @Singleton
-    @Provides
-    Handler providesHandler()
-    {
-        return new Handler(Looper.getMainLooper());
-    }
-
-    @Singleton
-    @Provides
-    AppExecutors.MainThreadExecutor getMainThreadExecutor()
-    {
-        return new AppExecutors.MainThreadExecutor();
-    }
-
-    @Singleton
-    @Provides
-    AppExecutors getAppExecutors(AppExecutors.MainThreadExecutor mainThreadExecutor)
-    {
-        return new AppExecutors(Executors.newSingleThreadExecutor(),
-                Executors.newFixedThreadPool(3)
-                ,mainThreadExecutor);
-    }
-
 }
--- app/src/main/java/com/example/personal/clouds/dagger2/modules/WeatherRepositoryModule.java
+++ app/src/main/java/com/example/personal/clouds/dagger2/modules/WeatherRepositoryModule.java
@@ -1,9 +1,14 @@
 package com.example.personal.clouds.dagger2.modules;
 
+import android.os.Handler;
+import android.os.Looper;
+
 import com.example.personal.clouds.AppExecutors;
 import com.example.personal.clouds.data.WeatherRepository;
 import com.example.personal.clouds.data.database.WeatherDao;
 import com.example.personal.clouds.data.network.WeatherNetworkDataSource;
+
+import java.util.concurrent.Executors;
 
 import dagger.Module;
 import dagger.Provides;
@@ -23,5 +28,26 @@ public class WeatherRepositoryModule {
         return new WeatherRepository(weatherDao,networkDataSource,appExecutors);
     }
 
+    
+    @Provides
+    Handler providesHandler()
+    {
+        return new Handler(Looper.getMainLooper());
+    }
 
+
+    @Provides
+    AppExecutors.MainThreadExecutor getMainThreadExecutor()
+    {
+        return new AppExecutors.MainThreadExecutor();
+    }
+
+
+    @Provides
+    AppExecutors getAppExecutors(AppExecutors.MainThreadExecutor mainThreadExecutor)
+    {
+        return new AppExecutors(Executors.newSingleThreadExecutor(),
+                Executors.newFixedThreadPool(3)
+                ,mainThreadExecutor);
+    }
 }
